9d68f593d863b3475509d8603faeecbf22545317
Jay200123 <kenken.ayonon@gmail.com>
2024-07-14 12:01:03 +0800

    Add nodemail.js config

---
+++ src/config/nodemail.js
@@ -0,0 +1,13 @@
+import { createTransport } from "nodemailer";
+import { ENV } from "../config/index.js";
+import { RESOURCE } from "../constants/index.js"; 
+
+const transporter = createTransport({
+  service: RESOURCE.GMAIL,
+  auth: {
+    user: ENV.EMAIL,
+    pass: ENV.EMAIL_PASSWORD,
+  },
+});
+
+export { transporter };
