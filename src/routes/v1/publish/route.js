59db09aebb6e9143855efd05cb04ead8d2465ae9
Jay200123 <kenken.ayonon@gmail.com>
2024-07-14 12:02:48 +0800

    Add publish route

---
+++ src/routes/v1/publish/route.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import * as publishController from "./controller.js";
+import { METHOD, PATH, ROLE } from "../../../constants/index.js";
+import { verifyJWT, authorizeRoles } from "../../../middlewares/index.js";
+
+const router = Router();
+
+const linkRoutes = [
+    {
+        method: METHOD.GET,
+        handler: publishController.getAllLinks,
+    },
+    {
+        method: METHOD.GET,
+        path: PATH.ID,
+        roles: [ROLE.ADMIN],
+        middleware: [verifyJWT],
+        handler: publishController.getSingleLink,
+    },
+    {
+        method: METHOD.POST,
+        handler: publishController.createPublishLink,
+    },
+    {
+        method: METHOD.PATCH,
+        path: PATH.EDIT,
+        roles: [ROLE.ADMIN],
+        middleware: [verifyJWT],
+        handler: publishController.updatePublishLink,
+    },
+    {
+        method: METHOD.DELETE,
+        path: PATH.DELETE,
+        handler: publishController.deletedLink
+    }
+];
+
+linkRoutes.forEach((route)=>{
+    const {  method, path = "", roles = [], middleware = [], handler } = route;
+    router[method](path, middleware.concat(authorizeRoles(...roles)), handler);
+
+});
+
+export default router;
